9e71c2b4fdef2c7b36bc52f20e61ec3e8c712de8
ariunbilig <arierdene0@gmail.com>
2025-08-14 20:58:18 +0800

    SplitText adjust +Each

---
+++ SplitText/EachWord/App.jsx
@@ -0,0 +1,19 @@
+import Each from './SplitTextEach';
+
+
+function App() {
+
+  return (
+    <>
+
+
+    <Each delay={0.5}>
+`     <h1>Timeless Form</h1>
+    </Each>`
+
+
+    </>
+  );
+}
+
+export default App;
